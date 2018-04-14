e53abd4278dc234b1f887f19f470540cfef46793
Ben Hanna <benpaulhanna@gmail.com>
2018-04-14 19:44:41 +0000

    Added Knex configuration and initial migration.

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    debug: true,
+    useNullAsDefault: true,
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    migrations: {
+      tableName: 'knexMigrations'
+    }
+  }
+};
---
+++ migrations/20180414185232_setup.js
@@ -0,0 +1,95 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('Permission', table => {
+      table
+        .integer('id')
+        .notNullable()
+        .primary();
+        
+      table.string('name');
+    }),
+    
+    knex.schema.createTable('Role', table => {
+      table
+        .integer('id')
+        .notNullable()
+        .primary();
+        
+      table
+        .integer('parentId')
+        .references('Role.id');
+  
+      table
+        .string('name')
+        .notNullable();
+    }),
+    
+    knex.schema.createTable('RolePermission', table => {
+      table
+        .integer('id')
+        .notNullable()
+        .primary();
+        
+      table
+        .integer('permissionId')
+        .references('Permission.id');
+        
+      table
+        .integer('roleId')
+        .references('Role.id');
+        
+      table
+        .boolean('isRevoked')
+        .notNullable()
+        .defaultTo(0);
+        
+      table.unique(['permissionId', 'roleId']);
+    }),
+    
+    knex.schema.createTable('user', table => {
+      table
+        .integer('id')
+        .notNullable()
+        .primary();
+      
+      table
+        .string('hash')
+        .notNullable();
+      
+      table
+        .dateTime('dateAdded')
+        .defaultTo(knex.fn.now());
+    }),
+  
+    knex.schema.createTable('UserProfile', table => {
+      table
+        .integer('id')
+        .notNullable()
+        .primary()
+        .references('User.id');
+      
+      table
+        .integer('roleId')
+        .notNullable()
+        .references('Role.id');
+      
+      table
+        .string('email')
+        .notNullable();
+      
+      table.string('firstName');
+      table.string('lastName');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('Permission'),
+    knex.schema.dropTable('Role'),
+    knex.schema.dropTable('RolePermission'),
+    knex.schema.dropTable('UserProfile'),
+    knex.schema.dropTable('User')
+  ]);
+};
