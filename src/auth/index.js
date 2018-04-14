9456be3857eca7d89ddccbe80a9114751120e940
Ben Hanna <benpaulhanna@gmail.com>
2018-04-14 20:50:58 +0000

    Added router for authentication.

---
+++ src/auth/index.js
@@ -0,0 +1,9 @@
+const Router = require('koa-router');
+
+const router = new Router();
+
+router.post('/auth', ctx => {
+  
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const Koa = require('koa');
+const authRouter = require('./auth');
 const graphqlRouter = require('./graphql');
 
 const app = new Koa();
+
+app.use(authRouter.route());
+app.use(authRouter.allowedMethods());
 
 app.use(graphqlRouter.routes());
 app.use(graphqlRouter.allowedMethods());
