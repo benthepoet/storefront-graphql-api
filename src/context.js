57786f2bfe203f93d70efcdbdba4d4fedf60ea63
Ben Hanna <benpaulhanna@gmail.com>
2018-04-15 16:53:27 +0000

    Added whitespace.

--- src/context.js
+++ src/context.js
@@ -7,10 +7,14 @@ const { UserService } = require('./services');
 
 module.exports.buildContext = (user) => {
   const bottle = new Bottle();
+  
   bottle.value('config', config);
   bottle.value('knex', knex);
   bottle.value('user', user);
+  
   bottle.service('UserService', UserService, 'config', 'knex');
+  
   bottle.service('UserController', UserController, 'UserService');
+  
   return bottle.container;
 };
